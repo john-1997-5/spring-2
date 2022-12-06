325a42c2afca9581c8728a3c98611a47be2aca30
Johnson <johnson.arrobo@gmail.com>
2022-12-06 21:50:48 +0100

    Authentication desde BD y no inMemory

--- src/main/java/com/johnson/spring/config/ProjectSecurityConfig.java
+++ src/main/java/com/johnson/spring/config/ProjectSecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,18 +17,6 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        // permitir acceso a todas las requests
-/*        return http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-                .httpBasic(Customizer.withDefaults())
-                .build();*/
-
-        // denegar acceso a todas las requests
-/*        return http.authorizeHttpRequests(auth -> auth.anyRequest().denyAll())
-                .httpBasic(Customizer.withDefaults())
-                .build();*/
-
-        // se permite el acceso a unas páginas, pero para otras se necesita autenticarse
-
         http.csrf()
                 .ignoringAntMatchers("/saveMsg") // desactiva csrf para este endpoint
                 .ignoringAntMatchers("/public/**")
@@ -54,23 +44,5 @@ public class ProjectSecurityConfig {
                 }
         );
         return http.build();
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("steve")
-                .password("steve")
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("johnson")
-                .password("johnson")
-                .roles("ADMIN")
-                .build();
-
-
-        return new InMemoryUserDetailsManager(user, admin);
     }
 }
---
+++ src/main/java/com/johnson/spring/security/MyAuthProvider.java
@@ -0,0 +1,48 @@
+package com.johnson.spring.security;
+
+import com.johnson.spring.model.entities.Person;
+import com.johnson.spring.model.entities.Role;
+import com.johnson.spring.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@Component
+public class MyAuthProvider implements AuthenticationProvider {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String pass = authentication.getCredentials().toString();
+        Person person = personRepository.readByEmail(email);
+        if (person != null && pass.equals(person.getPwd())) {
+            return new UsernamePasswordAuthenticationToken(person.getName(), pass, getGrantedAuthorities(person.getRole()));
+        }
+        throw new BadCredentialsException("credenciales inválidas");
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(Role role) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getRoleName()));
+        return authorities;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return authentication.equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
